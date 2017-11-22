8b8fa44a2bd515afdd5298b75b0c6cf663193bef
Giorgi Fafakerashvili <george.fafakerashvili@azry.com>
2017-11-22 11:45:14 +0400

    first commit

---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_47424.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_47424.dir/testCXXCompiler.cxx.obj"
+  "cmTC_47424.pdb"
+  "cmTC_47424.exe"
+  "cmTC_47424.exe.manifest"
+  "libcmTC_47424.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_47424.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
