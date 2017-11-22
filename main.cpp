a8721fcb0e2b832c067984afe3a0c9d4b1609ac2
Giorgi Fafakerashvili <george.fafakerashvili@azry.com>
2017-11-22 15:03:23 +0400

    added status.h status.cpp slice.h

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "include/export.h"
+
+#include "include/slice.h"
+
+
+void SliceTestInitialization() {
+    Slice slice;
+    assert(slice.size() == 0);
+}
+
+void SliceTest() {
+    const char* data = "giorgi";
+    Slice slice(data);
+
+    assert(memcmp(data, slice.data(), slice.size()) == 0);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    SliceTestInitialization();
+    SliceTest();
+
+
     return 0;
 }
